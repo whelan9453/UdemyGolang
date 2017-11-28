fef213a3d6db5562e7cacf7926701beb97c16ed6
whelan957 <sitdtest@gmail.com>
2017-11-28 10:47:38 +0800

    Add the instructor's sample code of the advanced interface practice.

--- HardInterface/main.go
+++ HardInterface/main.go
@@ -2,19 +2,28 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"os"
 )
 
 func main() {
-	argsWithProg := os.Args
+	// argsWithProg := os.Args
 	// fmt.Println("argsWithProg", argsWithProg)
-	byteSlice, err := ioutil.ReadFile(argsWithProg[1])
+	// byteSlice, err := ioutil.ReadFile(argsWithProg[1])
 
+	// if err != nil {
+	// 	fmt.Println("Error:", err)
+	// 	os.Exit(1)
+	// }
+
+	// fmt.Print(string(byteSlice[:]))
+
+	//Sample code from the instructor
+	f, err := os.Open(os.Args[1])
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 
-	fmt.Print(string(byteSlice[:]))
+	io.Copy(os.Stdout, f)
 }
